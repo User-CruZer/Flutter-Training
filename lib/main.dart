347a3f2910e24062c216ee0ead11317411e7ede2
User-CruZer <ahmadzakialbadri@gmail.com>
2026-04-04 19:26:51 +0700

    Training Flutter Fundamental pt3 (List View)

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
           title: Text("Welcome", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),),
-          backgroundColor: Colors.black,
+          backgroundColor: Colors.orange[600],
           elevation: 0,
           leading: Icon(Icons.menu, color: Colors.white,),
           actions: [
             IconButton(onPressed: () {}, icon: Icon(Icons.monetization_on, color: Colors.white,))
           ],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: ListView(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          scrollDirection: Axis.horizontal,
           children: [
             // 1st Row
-            Expanded(
-              child: Container(
-                color: Colors.orange[100],
-              ),
+            Container(
+              width: 350,
+              color: Colors.orange[100],
             ),
 
             // 2nd Row
-            Expanded(
-              flex: 3,
-              child: Container(
-                color: Colors.orange[300],
-              ),
+            Container(
+              width: 350,
+              color: Colors.orange[300],
             ),
+
             // 3rd Row
-            Expanded(
-              child: Container(
-                color: Colors.orange[500],
-              ),
+            Container(
+              width: 350,
+              color: Colors.orange[500],
             )
           ],
         ),

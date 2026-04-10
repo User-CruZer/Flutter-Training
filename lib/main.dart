a245f52435395352cc7107556fba509df1fdd926
User-CruZer <ahmadzakialbadri@gmail.com>
2026-04-10 11:42:04 +0700

    Training Flutter Fundamental Pt4 GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  List names = ["John", "Jane", "Doe", "Smith", "Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text("Welcome", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),),
           backgroundColor: Colors.orange[600],
@@ -22,50 +24,10 @@ class MyApp extends StatelessWidget {
             IconButton(onPressed: () {}, icon: Icon(Icons.monetization_on, color: Colors.white,))
           ],
         ),
-        body: ListView(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          // scrollDirection: Axis.horizontal,
-          children: [
-            // 1st Row
-            Container(
-              height: 350,
-              color: Colors.orange[100],
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.discord, size: 130, color: Colors.blue, shadows: [BoxShadow(color: Colors.black, offset: Offset(2, 2), blurRadius: 4)],),
-                  Text("Discord", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
-                ],
-              ),
-            ),
-
-            // 2nd Row
-            Container(
-              height: 350,
-              color: Colors.orange[300],
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.facebook, size: 130, color: Colors.blue, shadows: [BoxShadow(color: Colors.black, offset: Offset(2, 2), blurRadius: 4)],),
-                  Text("Facebook", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),)
-                ],
-              ),
-            ),
-
-            // 3rd Row
-            Container(
-              height: 350,
-              color: Colors.orange[500],
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.tiktok, size: 130, color: Colors.black, shadows: [BoxShadow(color: Colors.black, offset: Offset(2, 2), blurRadius: 4)],),
-                  Text("TikTok", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),)
-                ],
-              ),
-            )
-          ],
-        ),
+        body: GridView.builder(
+          itemCount: 64,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
+           itemBuilder: (context, index) => Container(color: Colors.blue, margin: EdgeInsets.all(4),))
       ),
     );
   }

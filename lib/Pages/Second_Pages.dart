7f911ea0840066e27fe44d6daee464babeb31c1c
User-CruZer <ahmadzakialbadri@gmail.com>
2026-04-12 12:44:55 +0700

    Training flutter fundamental Pt6 (Navigation Routes)

---
+++ lib/Pages/Second_Pages.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Pages/First_Pages.dart';
+
+class SecondPages extends StatelessWidget {
+  const SecondPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Second Page", style: TextStyle(color: Colors.white),),
+        ),
+      body: Center(child: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+        child: Text("Go To The First Page", style: TextStyle(color: Colors.white),),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => FirstPages()));
+        },
+      )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,19 @@
 import "package:flutter/material.dart";
+import "package:flutter_application_1/Pages/First_Pages.dart";
+import "package:flutter_application_1/Pages/Second_Pages.dart";
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  List names = ["John", "Jane", "Doe", "Smith", "Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
-
-  void userTapped() {
-    print("User tapped the container!");
-  }
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("Welcome", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),),
-          backgroundColor: Colors.orange[600],
-          elevation: 0,
-          leading: Icon(Icons.menu, color: Colors.white,),
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.monetization_on, color: Colors.white,))
-          ],
-        ),
-        body: Center(
-          child: GestureDetector(
-            onTap: userTapped,
-              // On tap, user tapped the container
-            child: Container(
-              height: 300,
-              width: 300,
-              color: Colors.orange[100],
-              child: Center(child: Text("Hello World"),),
-            ),
-          ),
-        )
-      ),
+      home: FirstPages(),
     );
   }
 }

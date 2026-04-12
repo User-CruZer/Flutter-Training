518e65297a1fd3c2d6c0d23e05c7da8c596399d0
User-CruZer <ahmadzakialbadri@gmail.com>
2026-04-12 12:20:48 +0700

    Training Flutter Fundamental Pt 5 (GestureDetector Method)

--- lib/main.dart
+++ lib/main.dart
@@ -24,38 +24,20 @@ class MyApp extends StatelessWidget {
             IconButton(onPressed: () {}, icon: Icon(Icons.monetization_on, color: Colors.white,))
           ],
         ),
-        body: Stack(children: [
-          // Background container
-          Container(
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.orange[600],
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              print("Container tapped!");
+              },
+              // On tap, user tapped the container
+            child: Container(
+              height: 300,
+              width: 300,
+              color: Colors.orange[100],
+              child: Center(child: Text("Hello World"),),
             ),
           ),
-          // List of names
-          ListView.builder(
-            padding: EdgeInsets.only(top: 20),
-            itemCount: names.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.orange[600],
-                  child: Text(names[index][0], style: TextStyle(color: Colors.white),),
-                ),
-                title: Text(names[index], style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
-                subtitle: Text("Subtitle for ${names[index]}"),
-                trailing: Icon(Icons.arrow_forward_ios, size: 16,),
-              );
-            },
-          ),
-          // Floating action button
-          Positioned(bottom: 20, right: 20, child: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.orange[600],
-            child: Icon(Icons.add, color: Colors.white,),
-          ),)
-        ],)
+        )
       ),
     );
   }
